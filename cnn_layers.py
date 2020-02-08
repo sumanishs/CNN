993fc23b88e9ff698953a8424b5cc70b7fc027b2
Sumanish <sumanish.cse.jgee@gmail.com>
2020-02-08 22:18:20 +0530

    Few CNN layers

--- cnn_layers.py
+++ cnn_layers.py
@@ -7,6 +7,9 @@ import numpy as np
 import argparse
 import random
 
+def calling(  ):
+    import sys
+    return sys._getframe(1).f_code.co_name
 
 def apply_kernel(array, kernel):
     return np.sum(np.multiply(array, kernel)) 
@@ -54,26 +57,45 @@ def conv_op_2d(input, kernel, hstride = 1, vstride = 1, stride = -1, pad = "NONE
 
     kh = kernel.shape[0]
     kw = kernel.shape[1]
+    iho = input.shape[0]
+    iwo = input.shape[1]
     if pad == "SAME":
         input = add_pad(input, hpad = kh-1, vpad = kw-1)
-        
-    oh = input.shape[0] - (kh-1)
-    ow = input.shape[1] - (kw-1)
-        
-    print "Output size:", oh , "x" , ow
+    ih = input.shape[0]
+    iw = input.shape[1]
+    oh = ((ih - kh)/hstride) + 1
+    ow = ((iw - kw)/vstride) + 1
+    name = calling() 
+    print "(", name, ")", "Output size:", oh , "x" , ow
     output = np.zeros([oh, ow], dtype = float)
-    for r in range(0, oh, vstride):
-        for c in range(0, ow, hstride):
-            part = copy_part(input, r, c, r+kh-1, c+kw-1)
-            conv_val = apply_kernel(part, kernel)
-            output[r][c] = conv_val
+    outr = -1
+    outc = -1		
+    for r in range(0, iho, vstride):
+	outr += 1
+	outc = -1
+        for c in range(0, iwo, hstride):
+	    outc += 1
+	    conv_val = 0.0
+            if r+kh-1 < ih and c+kw-1 < iw:		
+            	part = copy_part(input, r, c, r+kh-1, c+kw-1)
+            	conv_val = apply_kernel(part, kernel)
+            	output[outr][outc] = conv_val
     return output
+
+def rand_pixel(array):
+    h = array.shape[0]
+    w = array.shape[1]
+    for r in range(h):
+	for c in range(w):
+	    array[r][c] = random.randint(-2, 1)
+
 
 def fillarray_img(array, filler, count):
     if filler == 'inc':
         array.fill(count + 1)
     elif filler == 'rand':
-        array.fill(random.randint(1, 3))
+        array.fill(random.randint(1, 2))
+	rand_pixel(array)
     else:
         print "Wrong filler type.."
         exit()
@@ -122,14 +144,13 @@ def conv_op_3d(input, kernel, hstride = 1, vstride = 1, stride = -1, pad = "NONE
     iw = input.shape[2]
 
     if pad == "SAME":
-        oh = ih
-        ow = iw
-    else:
-        oh = ih - (kh - 1)
-        ow = iw - (kw - 1)
+        ih = ih + 2 * ((kh - 1)/2)
+        iw = iw + 2 * ((kw -1)/2)
+    
+    oh = ((ih - kh)/hstride) + 1
+    ow = ((iw - kw)/vstride) + 1
 
     print "Output (Row X Column):(", oh, "X", ow, ")"
-
     conv_tmp = np.zeros([oc, ic, oh, ow], dtype = float)
     
     for o in range(oc):
@@ -151,18 +172,119 @@ def conv_op_3d(input, kernel, hstride = 1, vstride = 1, stride = -1, pad = "NONE
                 conv[o][h][w] = val
     return conv
 
-def max_pool_3d(input, kernel_size = 2, stride = 2):
+def max_pool_3d(input, kh = 2, kw = 2, kernel_size = -1, hstride = 2, vstride = 2, stride = -1):
     print "Max pool input shape:", input.shape
     print "Kernel:", kernel_size, "X", kernel_size
-    print "Stride:", stride
+    if stride != -1:
+	hstride = stride
+	vstride = stride
+    print "HStride:", hstride
+    print "VStride:", vstride
+    oc = input.shape[0]
+    ih = input.shape[1]
+    iw = input.shape[2]
+    if kernel_size != -1:
+	kh = kernel_size
+	kw = kernel_size
+    
+    oh = ((ih - kh)/hstride) + 1
+    ow = ((iw - kw)/vstride) + 1
+
+    print ("Max pool output: %d x %d" % (oh, ow))	
+    pool_out = np.zeros([oc, oh, ow], dtype = float)
+    outr = -1
+    outc = -1	
+    for o in range(oc):
+	outr = -1	
+    	for r in range(0, ih, vstride):
+	    outr += 1
+	    outc = -1
+    	    for c in range(0, iw, hstride):
+		outc += 1
+    	        if r+kh-1 < ih and c+kw-1 < iw:		
+    	        	part = copy_part(input[o], r, c, r+kh-1, c+kw-1)
+			max_val = np.amax(part)	
+			#print part, "Max:", np.amax(part)
+			#print ("o:%d, outr:%d, outc:%d" % (o, outr, outc))
+			pool_out[o][outr][outc] = max_val
+    return pool_out 
+	
+    	
+def avg_pool_3d(input, kh = 2, kw = 2, kernel_size = -1, hstride = 2, vstride = 2, stride = -1):
+    print "Avg pool input shape:", input.shape
+    print "Kernel:", kernel_size, "X", kernel_size
+    if stride != -1:
+	hstride = stride
+	vstride = stride
+    print "HStride:", hstride
+    print "VStride:", vstride
+    oc = input.shape[0]
+    ih = input.shape[1]
+    iw = input.shape[2]
+    if kernel_size != -1:
+	kh = kernel_size
+	kw = kernel_size
+    
+    oh = ((ih - kh)/hstride) + 1
+    ow = ((iw - kw)/vstride) + 1
+
+    print ("Avg pool output: %d x %d" % (oh, ow))	
+    pool_out = np.zeros([oc, oh, ow], dtype = float)
+    outr = -1
+    outc = -1	
+    for o in range(oc):
+	outr = -1	
+    	for r in range(0, ih, vstride):
+	    outr += 1
+	    outc = -1
+    	    for c in range(0, iw, hstride):
+		outc += 1
+    	        if r+kh-1 < ih and c+kw-1 < iw:		
+    	        	part = copy_part(input[o], r, c, r+kh-1, c+kw-1)
+			mean_val = np.mean(part)	
+			#print part, "Mean:", np.mean(part)
+			#print ("o:%d, outr:%d, outc:%d" % (o, outr, outc))
+			pool_out[o][outr][outc] = mean_val
+    return pool_out 
+
+def ReLu(input):
+    oc = input.shape[0]
+    ih = input.shape[1]
+    iw = input.shape[2]
+    for o in range(oc):
+	for r in range(ih):
+	    for c in range(iw):
+                input[o][r][c] = max(0, input[o][r][c])		
+    return input	
+
+def Leaky_ReLu(input, alpha = 0.01):
+    oc = input.shape[0]
+    ih = input.shape[1]
+    iw = input.shape[2]
+    for o in range(oc):
+	for r in range(ih):
+	    for c in range(iw):
+                input[o][r][c] = max(alpha * input[o][r][c], input[o][r][c])		
+    return input	
+
+def dLeaky_ReLu(input, alpha = 0.01):
+    oc = input.shape[0]
+    ih = input.shape[1]
+    iw = input.shape[2]
+    for o in range(oc):
+	for r in range(ih):
+	    for c in range(iw):
+                input[o][r][c] = max(alpha, input[o][r][c])		
+    return input	
 
 def main(argv):
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-oc','--op_channels', default=1, help='Number of output channels', required=False)
     parser.add_argument('-ic','--ip_channels', default=1, help='Number of output channels', required=False)
-    parser.add_argument('-iw','--img_width', default=28, help='Input image width', required=False)
-    parser.add_argument('-ih','--img_height', default=28, help='Input image height', required=False)
+    parser.add_argument('-iw','--img_width', default=8, help='Input image width', required=False)
+    parser.add_argument('-ih','--img_height', default=8, help='Input image height', required=False)
     parser.add_argument('-k','--kernel', default=5, help='Kernel size', required=False)
+    parser.add_argument('-p','--padding', default='SAME', help='Padding(SAME/NONE)', required=False)
     parser.add_argument('-f','--filler', default='rand', help='Weight matrix filler type(mod/inc/rand)', required=False)
     args = parser.parse_args()
     
@@ -189,22 +311,32 @@ def main(argv):
                   [0,1,1,0,0]      
                  ])
     
-    image = np.zeros([ic, ih, iw], dtype = int)
+    image = np.zeros([ic, ih, iw], dtype = float)
     for i in range(ic):
         fillarray_img(image[i], filler, i)
 
-    weights = np.zeros([oc, ic, K, K], dtype = int)
+    weights = np.zeros([oc, ic, K, K], dtype = float)
     for i in range(oc):
         fillarray_wt(weights[i], filler)
    
     print "Image:\n", image
     print "Weights:\n", weights 
     
-    conv_out = conv_op_3d(image, weights, stride = 1, pad = "NONE")
+    conv_out = conv_op_3d(image, weights, stride = 1, pad = args.padding)
     print "Convolution output shape:", conv_out.shape
     print "Convolve output:\n", conv_out
+    
+    max_pool_out = max_pool_3d(conv_out, kernel_size = 2, stride = 2)
+    print "Max pool out shape:", max_pool_out.shape
+    print max_pool_out	
 
-    max_pool_3d(conv_out, kernel_size = 2, stride = 2)
+    avg_pool_out = avg_pool_3d(conv_out, kernel_size = 2, stride = 2)
+    print "Avg pool out shape:", avg_pool_out.shape
+    print avg_pool_out
+
+    relu_out = ReLu(avg_pool_out)
+    print "ReLu shape:", relu_out.shape
+    print relu_out			
      
 
 if __name__ == "__main__":
